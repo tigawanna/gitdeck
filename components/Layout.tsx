915838869aa520b2707dfd9a83aaa0fa3cc9baee
tigawanna <denniskinuthiaw@gmail.com>
2022-08-29 16:47:04 +0300

    added react query setup

---
+++ components/Layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface LayoutProps {
+
+}
+
+export const Layout: React.FC<LayoutProps> = ({}) => {
+return (
+ <div>
+
+ </div>
+);
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,41 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Layout } from "../components/Layout";
+import { useLocalStorge } from "./../utils/hooks/useLocalStorge";
+import GlobalContext from "../utils/context/GlobalsContext";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      retry: false,
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const local = useLocalStorge();
+  // console.log("local state ==== ",local?.state)
+
+  // console.log("initial value in local storage ==== ", value);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <GlobalContext.Provider
+        value={{ value: local?.state, updateValue: local?.dispatch }}
+      >
+        <Layout local={local}>
+          <Component {...pageProps} />
+          <ReactQueryDevtools />
+        </Layout>
+      </GlobalContext.Provider>
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
